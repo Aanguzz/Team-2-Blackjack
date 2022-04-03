012d54408a95d96e0acc98c2958a3335e14e79c3
Sam Malawy <sammalawy@gmail.com>
2022-04-03 13:50:53 -0500

    Added the UIS background for the form

--- CSC478Blackjack/BlackjackGUI/Form1.Designer.cs
+++ CSC478Blackjack/BlackjackGUI/Form1.Designer.cs
@@ -29,22 +29,18 @@ namespace CSC478Blackjack
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.StartGame = new System.Windows.Forms.Button();
             this.StayButton = new System.Windows.Forms.Button();
             this.HitButton = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
+            this.BetAmount = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // StartGame
             // 
-            this.StartGame.Location = new System.Drawing.Point(656, 292);
+            this.StartGame.Location = new System.Drawing.Point(67, 173);
             this.StartGame.Name = "StartGame";
             this.StartGame.Size = new System.Drawing.Size(75, 23);
             this.StartGame.TabIndex = 0;
@@ -71,59 +67,14 @@ namespace CSC478Blackjack
             this.HitButton.UseVisualStyleBackColor = true;
             this.HitButton.Click += new System.EventHandler(this.HitButton_Click);
             // 
-            // button1
+            // BetAmount
             // 
-            this.button1.Location = new System.Drawing.Point(117, 179);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(137, 229);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(117, 266);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(117, 291);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(102, 320);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(117, 350);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 8;
-            this.button6.Text = "button6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.BetAmount.Location = new System.Drawing.Point(67, 290);
+            this.BetAmount.Name = "BetAmount";
+            this.BetAmount.Size = new System.Drawing.Size(75, 23);
+            this.BetAmount.TabIndex = 3;
+            this.BetAmount.Text = "button1";
+            this.BetAmount.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
@@ -137,7 +88,7 @@ namespace CSC478Blackjack
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(653, 350);
+            this.label2.Location = new System.Drawing.Point(78, 79);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(64, 13);
             this.label2.TabIndex = 10;
@@ -147,15 +98,11 @@ namespace CSC478Blackjack
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1844, 1036);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BetAmount);
             this.Controls.Add(this.HitButton);
             this.Controls.Add(this.StayButton);
             this.Controls.Add(this.StartGame);
@@ -172,12 +119,7 @@ namespace CSC478Blackjack
         private System.Windows.Forms.Button StartGame;
         private System.Windows.Forms.Button StayButton;
         private System.Windows.Forms.Button HitButton;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button BetAmount;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
     }
