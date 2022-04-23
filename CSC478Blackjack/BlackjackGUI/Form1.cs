d4da9d3bf7cd1bac3f84fc1d4ad64f28da5de725
Sam Malawy <sammalawy@gmail.com>
2022-04-22 20:42:37 -0500

    Addition of Stay and Hit button functionality

--- CSC478Blackjack/BlackjackGUI/Form1.cs
+++ CSC478Blackjack/BlackjackGUI/Form1.cs
@@ -32,11 +32,82 @@ namespace CSC478Blackjack
 
         private void HitButton_Click(object sender, EventArgs e)
         {
+            if (DealCardToPlayer())
+            {
+                if (playerHand.GetTotal() == 21)
+                {
+                    DealersTurn();
+                    CheckWinner();
+                    DisplayDealerGraphics();
 
+                }
+                else if (playerHand.GetTotal() > 21 && !playerHand.HasAces())
+                {
+                    playerFunds.LostBet();
+                    DisplayDealerGraphics();
+                    DisplayPlayerGraphics();
+                    MessageBox.Show("You busted! Dealer wins.");
+                    CheckBankrupt();
+                }
+            }
+            else
+            {
+                HitButton.Enabled = false;
+            }
         }
+
+        
+
         private void StayButton_Click(object sender, EventArgs e)
         {
-
+            //take note of the current cards
+            //Compare the active card totals
+            //Grant the Player or Dealer the win
+            //Make the dealer draw if under 17
+            //Make the dealer stay on 17 or greater
+            DisplayDealerGraphics();
+            CurrentBetBox.Enabled = true;
+            if (playerHand.GetTotal() > 21)
+            {
+                playerFunds.LostBet();
+                DealerCheckAce();
+                DisplayDealerGraphics();
+                DisplayPlayerGraphics();
+                MessageBox.Show("You busted! Dealer wins.");
+                CheckBankrupt();
+            }
+            else
+            {
+                bool dealing = true;
+                while (dealing)
+                {
+                    if (dealerHand.GetTotal() >= 21)
+                    {
+                        if (!DealerCheckAce())
+                        {
+                            dealing = false;
+                        }
+                    }
+                    else
+                    {
+                        if (dealerHand.GetTotal() < 17 || dealerHand.GetTotal() < playerHand.GetTotal())
+                        {
+                            if (!DealCardToDealer())
+                            {
+                                dealing = false;
+                            }
+                        }
+                        if (dealerHand.GetTotal() >= 17 && dealerHand.GetTotal() >= playerHand.GetTotal())
+                        {
+                            if (!DealerCheckAce())
+                            {
+                                dealing = false;
+                            }
+                        }
+                    }
+                }
+                CheckWinner();
+            }
         }
         private void StartGame_Click(object sender, EventArgs e)
         {
@@ -117,17 +188,6 @@ namespace CSC478Blackjack
             //DealerLabelList[3] = dealerCardValue4;
             //DealerLabelList[4] = dealerCardValue5;
         }
-
-        private void CheckBankrupt()
-        {
-            if (playerFunds.GetTotalFunds() <= 0)
-            {
-                MessageBox.Show("You ran out of money. Game over!");
-                Application.Exit();
-            }
-        }
-       
-
         private void DiableCardButtons()
         {
             playerCard1.Enabled = false;
@@ -179,7 +239,127 @@ namespace CSC478Blackjack
             playerHand.SetTotal();
             DisplayPlayerGraphics();
         }
+        private void CheckBankrupt()
+        {
+            if (playerFunds.GetTotalFunds() <= 0)
+            {
+                MessageBox.Show("You ran out of money. Game over!");
+                Application.Exit();
+            }
+        }
+        private void CheckWinner()
+        {
+            if (playerHand.GetTotal() > dealerHand.GetTotal())
+            {
+                if (dealerHand.GetTotal() > 21)
+                {
+                    playerFunds.WonBet();
+                    DisplayPlayerGraphics();
+                    MessageBox.Show("Dealer's Hand:" + dealerHand.GetTotalString() + "\nPlayer's Hand:" + playerHand.GetTotalString() + "\n\nDealer Busts. You win!");
+                }
+                else
+                {
 
+                    playerFunds.WonBet();
+                    DisplayPlayerGraphics();
+                    MessageBox.Show("Dealer's Hand:" + dealerHand.GetTotalString() + "\nPlayer's Hand:" + playerHand.GetTotalString() + "\n\nYou have the better hand. You win!");
+                }
+
+            }
+            else if (playerHand.GetTotal() < dealerHand.GetTotal())
+            {
+                if (dealerHand.GetTotal() <= 21)
+                {
+                    playerFunds.LostBet();
+                    DisplayPlayerGraphics();
+                    MessageBox.Show("Dealer's Hand:" + dealerHand.GetTotalString() + "\nPlayer's Hand:" + playerHand.GetTotalString() + "\n\nDealer has the better hand. You lose.");
+                    CheckBankrupt();
+                }
+                else
+                {
+                    playerFunds.WonBet();
+                    DisplayPlayerGraphics();
+                    MessageBox.Show("Dealer's Hand:" + dealerHand.GetTotalString() + "\nPlayer's Hand:" + playerHand.GetTotalString() + "\n\nDealer Busts. You win!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Dealer's Hand:" + dealerHand.GetTotalString() + "\nPlayer's Hand:" + playerHand.GetTotalString() + "\n\nTie. Nothing happens.");
+            }
+            DiableCardButtons();
+        }
+        private bool DealerCheckAce()
+        {
+            if (dealerHand.GetTotal() > 21 && dealerHand.HasAces())
+            {
+                for (int i = 0; i < dealerHand.GetNumberofCards(); i++)
+                {
+                    if (dealerHand.GetCard(i).GetAce() && dealerHand.GetCard(i).GetValue() == 11)
+                    {
+                        dealerHand.GetCard(i).ToggleAce();
+                        dealerHand.SetTotal();
+                        DisplayDealerGraphics();
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        private void DealersTurn()
+        {
+            bool dealing = true;
+            while (dealing)
+            {
+                if (dealerHand.GetTotal() >= 21)
+                {
+                    if (!DealerCheckAce())
+                    {
+                        dealing = false;
+                    }
+                }
+                else
+                {
+                    if (dealerHand.GetTotal() < 17 || dealerHand.GetTotal() < playerHand.GetTotal())
+                    {
+                        if (!DealCardToDealer())
+                        {
+                            dealing = false;
+                        }
+                    }
+                    if (dealerHand.GetTotal() >= 17 && dealerHand.GetTotal() >= playerHand.GetTotal())
+                    {
+                        if (!DealerCheckAce())
+                        {
+                            dealing = false;
+                        }
+                    }
+                }
+            }
+        }
+        private Boolean DealCardToDealer()
+        {
+            int numberofcards = dealerHand.GetNumberofCards();
+            if (numberofcards < 5)
+            {
+                Card somecard = deck.GetNextCard();
+                dealerHand.DealCard(somecard);
+                DisplayDealerGraphics();
+                return true;
+            }
+            return false;
+        }
+        private Boolean DealCardToPlayer()
+        {
+            int numberofcards = playerHand.GetNumberofCards();
+            if (numberofcards < 5)
+            {
+                Card somecard = deck.GetNextCard();
+                playerHand.DealCard(somecard);
+                DisplayPlayerGraphics();
+                return true;
+            }
+            return false;
+        }
         public void DisplayDealerGraphics()
         {
             if (firstHand)
